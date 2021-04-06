6753080578ef313a0083ac47d20c62a349759dc3
ElluLaitinen <ellu.laitinen85@gmail.com>
2021-04-06 16:49:36 +0300

    add api

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
 import {
   Grid,
   Card,
@@ -8,31 +9,71 @@ import {
   TextField,
   TextareaAutosize,
   Button,
+  Divider,
 } from "@material-ui/core";
+import { listRecipes } from "./graphql/queries";
+import {
+  createRecipe as createRecipeMutation,
+  deleteRecipe as deleteRecipeMutation,
+} from "./graphql/mutations";
 
 const AddRecipe = () => {
-  const [ingredients, setIngredients] = useState([]);
-  const [newRecipe, setNewRecipe] = useState({
-    title: { value: "" },
-    ingredients: ingredients,
-    instructions: { value: "" },
-  });
+  // const [ingredients, setIngredients] = useState([]);
+  const initialState = {
+    title: "",
+    ingredients: "",
+    instructions: "",
+  };
+  const [recipes, setRecipes] = useState([]);
+  const [recipeData, setRecipeData] = useState(initialState);
 
-  const saveData = ({ name, value }) => {
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listRecipes });
+    setRecipes(apiData.data.listRecipes.items);
+  }
+  async function createNote() {
+    if (
+      !recipeData.title ||
+      !recipeData.ingredients ||
+      !recipeData.instructions
+    )
+      return;
+    await API.graphql({
+      query: createRecipeMutation,
+      variables: { input: recipeData },
+    });
+    setRecipes([...recipes, recipeData]);
+    setRecipeData(initialState);
+  }
+
+  async function deleteNote({ id }) {
+    const newRecipesArray = recipes.filter((note) => note.id !== id);
+    setRecipes(newRecipesArray);
+    await API.graphql({
+      query: deleteRecipeMutation,
+      variables: { input: { id } },
+    });
+  }
+
+  /*  const saveData = ({ name, value }) => {
     setNewRecipe({
       ...newRecipe,
       [name]: value,
     });
-  };
+  }; */
 
-  const changeIngHandler = (e) => {
+  /*   const changeIngHandler = (e) => {
     setIngredients({
       ...ingredients,
       [e.target.name]: e.target.value,
     });
-  };
+  }; */
 
-  const addIng = (e) => {
+  /*   const addIng = (e) => {
     e.preventDefault();
     saveData({
       name: "ingredients",
@@ -41,16 +82,16 @@ const AddRecipe = () => {
     console.log(ingredients);
   };
   console.log(newRecipe);
-  console.log(ingredients);
-
+  console.log(ingredients); */
+  /* 
   const changeHandler = (e) => {
     setNewRecipe({
       ...newRecipe,
       [e.target.name]: e.target.value,
     });
-  };
+  }; */
 
-  const addRecipe = (e) => {
+  /*   const addRecipe = (e) => {
     e.preventDefault();
     console.log("new recipe added");
     console.log(newRecipe);
@@ -60,8 +101,8 @@ const AddRecipe = () => {
         console.log(response.data);
       });
   };
-
-  const removeIng = (ingredients) => {
+ */
+  /*   const removeIng = (ingredients) => {
     console.log(ingredients);
     const newIngList = newRecipe.ingredients.filter(
       (x) => x.ingredients !== ingredients
@@ -70,7 +111,7 @@ const AddRecipe = () => {
       name: "ingredients",
       value: [...newIngList],
     });
-  };
+  }; */
 
   return (
     <Card style={{ margin: "2rem" }}>
@@ -82,7 +123,10 @@ const AddRecipe = () => {
             size="small"
             type="text"
             name="title"
-            onChange={changeHandler}
+            value={recipeData.title}
+            onChange={(e) =>
+              setRecipeData({ ...recipeData, title: e.target.value })
+            }
           ></TextField>
         </Grid>
         <Grid item>
@@ -92,28 +136,42 @@ const AddRecipe = () => {
             size="small"
             type="text"
             name="ingredients"
-            onChange={changeIngHandler}
+            value={recipeData.ingredients}
+            onChange={(e) =>
+              setRecipeData({ ...recipeData, ingredients: e.target.value })
+            }
           ></TextField>
-          <Button onClick={addIng}>Lisää listaan</Button>
+          {/*  <Button onClick={addIng}>Lisää listaan</Button> */}
         </Grid>
-        {newRecipe.ingredients.map((i) => (
+        {/*       {newRecipe.ingredients.map((i) => (
           <div>
             <li value={i.ingredients}>{i.ingredients}</li>
             <button onClick={() => removeIng(i.ingredients)}>poista</button>
           </div>
-        ))}
+        ))} */}
         <Grid item>
           <InputLabel>Ohjeet</InputLabel>
           <TextareaAutosize
             rowsMin={10}
             type="text"
             name="instructions"
-            onChange={changeHandler}
+            value={recipeData.instructions}
+            onChange={(e) =>
+              setRecipeData({ ...recipeData, instructions: e.target.value })
+            }
           ></TextareaAutosize>
         </Grid>
-        <Button type="submit" onClick={addRecipe}>
+        <Button type="submit" onClick={createNote}>
           Lisää resepti
         </Button>
+        {recipes.map((recipe) => (
+          <div key={recipe.id || recipe.name}>
+            <h2>{recipe.title}</h2>
+            <p>{recipe.ingredients}</p>
+            <p>{recipe.instructions}</p>
+            <Button onClick={() => deleteNote(recipe)}>Poista resepti</Button>
+          </div>
+        ))}
       </Grid>
     </Card>
   );
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateRecipe = /* GraphQL */ `
+  subscription OnCreateRecipe {
+    onCreateRecipe {
+      id
+      title
+      ingredients
+      instructions
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateRecipe = /* GraphQL */ `
+  subscription OnUpdateRecipe {
+    onUpdateRecipe {
+      id
+      title
+      ingredients
+      instructions
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteRecipe = /* GraphQL */ `
+  subscription OnDeleteRecipe {
+    onDeleteRecipe {
+      id
+      title
+      ingredients
+      instructions
+      createdAt
+      updatedAt
+    }
+  }
+`;
